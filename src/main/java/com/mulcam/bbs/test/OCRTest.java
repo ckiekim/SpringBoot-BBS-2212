f41fff1683e227f2fe1464b7d1e61974290cebc7
Cheolkie Kim <ckiekim@naver.com>
2023-02-02 10:50:05 +0900

    OCR

--- src/main/java/com/mulcam/bbs/test/OCRTest.java
+++ src/main/java/com/mulcam/bbs/test/OCRTest.java
@@ -8,6 +8,8 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import org.json.JSONArray;
@@ -16,11 +18,9 @@ import org.json.JSONObject;
 public class OCRTest {
 
 	public static void main(String[] args) throws Exception {
-//		String apiURL = "https://r77a72wf4p.apigw.ntruss.com/custom/v1/20383/a0a44ed8cfc32b7e61befeb99bbff7706808c3fe4dcdf4750a8addb3ffcd4008/general";
-//		String secretKey = "V1JpVHpDV3hjaGdtQlBkeWlyR0FSeldnRFhLbG9MUnM=";
 		String apiURL = "https://ei72792oci.apigw.ntruss.com/custom/v1/20382/cdfc03af341c21e0171808b9398cd32e720127e2410ff211250f958135081153/general";
 		String secretKey = "bUlaS2pvdHlHSnJFYVhqdllXdmhwWnlybHBEd216WFU=";
-		String imageFile = "c:/Temp/자바_표지.jpg";
+		String imageFile = "c:/Temp/영수증2.jpg";
 
 		URL url = new URL(apiURL);
 		HttpURLConnection con = (HttpURLConnection)url.openConnection();
@@ -67,25 +67,40 @@ public class OCRTest {
 		br.close();
 
 		System.out.println(response);
+		json = new JSONObject(response.toString());
+		JSONObject result = (JSONObject) ((JSONArray) json.getJSONArray("images")).getJSONObject(0);
+		JSONArray fields = (JSONArray) result.getJSONArray("fields");
+		List<String> list = new ArrayList<>();
+		int count = 1;
+		for (Object field: fields) {
+			JSONObject jsonField = (JSONObject) field;
+			String text = jsonField.getString("inferText");
+			list.add(text);
+			System.out.print(text + ", ");
+			if (count % 10 == 0)
+				System.out.println();
+			count++;
+		}
 	}
 
 	private static void writeMultiPart(OutputStream out, String jsonMessage, File file, String boundary) throws Exception {
 		StringBuilder sb = new StringBuilder();
-		sb.append("--").append(boundary).append("\r\n");
-		sb.append("Content-Disposition:form-data; name=\"message\"\r\n\r\n");
+		String crlf = "\r\n";
+		sb.append("--").append(boundary).append(crlf);
+		sb.append("Content-Disposition:form-data; name=\"message\"" + crlf + crlf);
 		sb.append(jsonMessage);
-		sb.append("\r\n");
+		sb.append(crlf);
 	
 		out.write(sb.toString().getBytes("UTF-8"));
 		out.flush();
 	
 		if (file != null && file.isFile()) {
-			out.write(("--" + boundary + "\r\n").getBytes("UTF-8"));
+			out.write(("--" + boundary + crlf).getBytes("UTF-8"));
 			StringBuilder fileString = new StringBuilder();
 			fileString
 				.append("Content-Disposition:form-data; name=\"file\"; filename=");
-			fileString.append("\"" + file.getName() + "\"\r\n");
-			fileString.append("Content-Type: application/octet-stream\r\n\r\n");
+			fileString.append("\"" + file.getName() + "\"" + crlf);
+			fileString.append("Content-Type: application/octet-stream" + crlf + crlf);
 			out.write(fileString.toString().getBytes("UTF-8"));
 			out.flush();
 	
@@ -95,10 +110,10 @@ public class OCRTest {
 				while ((count = fis.read(buffer)) != -1) {
 					out.write(buffer, 0, count);
 				}
-				out.write("\r\n".getBytes());
+				out.write(crlf.getBytes());
 			}
 	
-			out.write(("--" + boundary + "--\r\n").getBytes("UTF-8"));
+			out.write(("--" + boundary + "--" + crlf).getBytes("UTF-8"));
 		}
 		out.flush();
 	}
